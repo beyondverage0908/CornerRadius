701f66ac869173e715ac304254e8a18051a50c1d
pingjun lin <beyondaverage0908@gmail.com>
2016-11-10 14:35:11 +0800

    add 3d touch func

---
+++ CornerRadius/ThreeDTouchController.h
@@ -0,0 +1,13 @@
+//
+//  ThreeDTouchController.h
+//  CornerRadius
+//
+//  Created by pingjun lin on 2016/11/10.
+//  Copyright © 2016年 linsir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThreeDTouchController : UIViewController
+
+@end
